54c74f971908256faa2e07a62b6364e044ca4f15
DimaFr <dima.friedman@gmail.com>
2014-07-31 22:55:20 +0300

    --

---
+++ projects/Angular Google Maps ex1/map.geolocation.service.js
@@ -0,0 +1,15 @@
+/**
+ * Created by admin on 7/30/14.
+ */
+angular.module('myMap')
+.factory({"GeolocationFactory":geolocationFactory})
+function geolocationFactory(){
+
+    var currentLocation =
+
+    function currentLocation
+
+    return{
+        CurrentLocation: currentLocation
+    };
+}
